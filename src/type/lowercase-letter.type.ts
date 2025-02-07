515ac8b8603621d7f1c731168f0e3a4725cd501b
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-07 21:06:43 +0000

    chore: move to the `@typedly`.

--- src/type/lowercase-letter.type.ts
+++
@@ -1,2 +0,0 @@
-// Type for lowercase letters from 'a' to 'z'
-export type LowercaseLetter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z';
--- src/type/numeric-letter.type.ts
+++
@@ -1,2 +0,0 @@
-// Type for numeric digits from '0' to '9'
-export type NumericLetter = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
